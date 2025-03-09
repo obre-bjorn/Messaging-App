250d85eb7af100170d35c2a51e7154bbbe75ff7c
obre-bjorn <obre.bjorn@gmail.com>
2025-03-09 23:41:10 +0300

    removed unecessary folder and file

--- server/routes/dist/groupChatRouts.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var express = require('express');
-
-var groupChatControllers = require('../controllers/groupchatController');
-
-var router = express.Router();
-module.exports = router;
