2c29dd891931d90b8a6a5a50a23cc92ba5a48bfc
obre-bjorn <obre.bjorn@gmail.com>
2025-03-22 00:31:48 +0300

    Implemented the auth services

---
+++ client/app/services/authService.ts
@@ -0,0 +1,28 @@
+import api from "./api";
+import type { User } from "~/types";
+
+
+
+interface AuthResponse {
+    token: string,
+    user : User
+}
+
+
+export const login  = async (username: string, password : string): Promise<AuthResponse> => {
+
+    const response = await api.post('/login', {username,password}) 
+    return response.data
+
+}
+
+
+export const register  = async (email : string ,username: string, password : string): Promise<AuthResponse> => {
+
+    const response = await api.post('/register', {email,username,password}) 
+    return response.data
+
+}
+
+
+//! Implement to validate token in the backend 
